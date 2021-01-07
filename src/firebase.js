07bf8789ac0a196d99e53dc020137f97475a024a
gilbertlc <gilbert.lacasandile@gmail.com>
2021-01-07 21:16:05 +0800

    wip

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAjNh2B3PmUiXVIsGp8-YmBTTXpKqXpFdQ',
+  authDomain: 'challenge-bf363.firebaseapp.com',
+  projectId: 'challenge-bf363',
+  storageBucket: 'challenge-bf363.appspot.com',
+  messagingSenderId: '847872087417',
+  appId: '1:847872087417:web:ad82e28702db84bdf94ba8',
+  measurementId: 'G-H648M3BGL7',
+};
