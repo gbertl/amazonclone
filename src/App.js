d1c7c10c61dc4e6b062154e89e0de07d5694d41f
gilbertlc <gilbert.lacasandile@gmail.com>
2021-01-09 19:37:08 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
-    </div>
+
+      <Switch>
+        <Route path="/checkout">
+          <div className="App">
+            <Checkout />
+          </div>
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <Home />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import './Checkout.css';
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <p>Body</p>
+    </div>
+  );
+}
+
+export default Checkout;
