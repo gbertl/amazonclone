84124a576d4308aa3aec1a41524c1dfee75e7f5f
gilbertlc <gilbert.lacasandile@gmail.com>
2021-01-31 20:03:18 +0800

    move user reducer -> src/reducers/user.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,4 @@
-export const initialState = {
-  user: null,
-};
+export const initialState = {};
 
 export const getBasketTotal = (basket) =>
   basket?.reduce((amount, item) => item.price + amount, 0);
@@ -9,13 +7,6 @@ const reducer = (state, action) => {
   console.log(action);
 
   switch (action.type) {
-    case "SET_USER":
-      return {
-        ...state,
-        user: action.user,
-      };
-      break;
-
     default:
       return state;
   }
